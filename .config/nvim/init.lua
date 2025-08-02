f5d276a4bf7d2cf93b772e2f657a5f2150c0810c
Pume Tuchinda <pumetuchinda@gmail.com>
2025-08-02 22:41:59 +0700

    update for clipboard ssh

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,3 +35,20 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- clipboard ssh
+vim.schedule(function()
+  vim.opt.clipboard:append('unnamedplus')
+
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end)
