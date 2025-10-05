537b3609666f6b3fd856f8144c934784b1ec917c
Pume Tuchinda <pumetuchinda@gmail.com>
2025-10-05 15:03:09 +0700

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    eval (ssh-agent -c) > /dev/null
+    ssh-add ~/.ssh/github 2>/dev/null
 end
-
-set -gx PATH /Users/pumetuchinda/.rubies/ruby-3.4.1/bin $PATH
